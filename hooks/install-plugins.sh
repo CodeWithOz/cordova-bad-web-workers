fd1a9e320b817bd5c7672cf26b972bccc7913a95
Uche Ozoemena <ucheozoemena@gmail.com>
2020-05-13 08:29:48 +0100

    Install google maps plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -3,3 +3,6 @@ cordova plugin add cordova-plugin-whitelist@1.3.4
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
+
+# install google maps plugin
+cordova plugin add cordova-plugin-googlemaps
