be07c893a67cc7a1e72b0bb4bd85897aeb1c67d7
Uche Ozoemena <ucheozoemena@gmail.com>
2020-05-13 19:00:37 +0100

    Install plugin from multiple maps branch and specify install variables

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -5,4 +5,6 @@ cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 
 # install google maps plugin
-cordova plugin add cordova-plugin-googlemaps
+cordova plugin add https://github.com/mapsplugin/cordova-plugin-googlemaps.git#multiple_maps \
+    --variable PLAY_SERVICES_VERSION=15.0.1 \
+    --variable ANDROID_SUPPORT_V4_VERSION=27.+
