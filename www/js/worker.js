666e4c5362cadfbd99bdfa1fc524f14dd7400d10
Uche Ozoemena <ucheozoemena@gmail.com>
2020-05-15 09:54:05 +0100

    Use correct relative path for scripts imported in worker

--- www/js/worker.js
+++ www/js/worker.js
@@ -1,5 +1,5 @@
-importScripts('./js/proxy.min.js');
-importScripts('./js/comlink/comlink.global.min.js');
+importScripts('./proxy.min.js');
+importScripts('./comlink/comlink.global.min.js');
 
 const workerService = {
     testWorker: function(cloneableValue) {
