779ea2967e9c703d5a8ed1c8a96f175e4fca6e83
Uche Ozoemena <ucheozoemena@gmail.com>
2020-06-27 16:21:44 +0100

    Install latest plugin from multiple_maps branch (commit #a9088cf)

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -5,6 +5,6 @@ cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 
 # install google maps plugin
-cordova plugin add https://github.com/mapsplugin/cordova-plugin-googlemaps.git#multiple_maps \
+cordova plugin add https://github.com/mapsplugin/cordova-plugin-googlemaps.git#a9088cf \
     --variable PLAY_SERVICES_VERSION=15.0.1 \
     --variable ANDROID_SUPPORT_V4_VERSION=27.+
