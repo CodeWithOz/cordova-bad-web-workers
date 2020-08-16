2e53dfc55fc2564d555109aeaf42bc05ffea6c4a
Uche Ozoemena <ucheozoemena@gmail.com>
2020-08-16 18:50:48 +0100

    Stop specifying maps plugin installation variables

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -5,6 +5,4 @@ cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 
 # install google maps plugin
-cordova plugin add cordova-plugin-googlemaps@2.7.1 --save \
-    --variable PLAY_SERVICES_VERSION=15.0.1 \
-    --variable ANDROID_SUPPORT_V4_VERSION=27.+
+cordova plugin add cordova-plugin-googlemaps@2.7.1 --save
