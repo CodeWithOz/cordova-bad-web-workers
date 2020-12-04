e901dfc39556ce2bffa45a49aee39aae35ec3280
Uche Ozoemena <ucheozoemena@gmail.com>
2020-12-04 11:28:51 +0100

    Add the plugins that may cause a flash

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -3,3 +3,13 @@ cordova plugin add cordova-plugin-whitelist@1.3.4
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
+cordova plugin add cordova-plugin-android-notch@1.0.3 --save
+cordova plugin add cordova-plugin-android-permissions@1.1.0 --save
+cordova plugin add cordova-plugin-device@2.0.3 --save
+cordova plugin add cordova-plugin-ionic-keyboard@2.2.0 --save
+cordova plugin add cordova-plugin-ionic-keyboard@2.2.0 --save
+cordova plugin add cordova-plugin-ionic-webview@5.0.0 --save
+cordova plugin add cordova-plugin-network-information@2.0.2 --save
+cordova plugin add cordova-plugin-splashscreen@6.0.0 --save
+cordova plugin add cordova.plugins.diagnostic@6.0.2 --save
+cordova plugin add es6-promise-plugin@4.2.2 --save
