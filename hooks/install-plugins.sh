aa0e0a795fdc36ddd46f62881bc5217728eb363f
Uche Ozoemena <ucheozoemena@gmail.com>
2021-10-02 01:20:20 +0100

    feat(core): install the file-transfer plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -3,3 +3,4 @@ cordova plugin add cordova-plugin-whitelist@1.3.4
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
+cordova plugin add https://github.com/apache/cordova-plugin-file-transfer#ec11d2fd0e87d7d9e0ba94b1989ffb71af1c3c96 --save
