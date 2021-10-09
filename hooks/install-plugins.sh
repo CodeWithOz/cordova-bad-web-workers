8290c3e1bd71cdf631b1971e646d5c770e4fe34f
Uche Ozoemena <ucheozoemena@gmail.com>
2021-10-09 10:32:21 +0100

    feat(core): install the native logs plugin from Uche's fork

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -5,3 +5,5 @@ cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 cordova plugin add cordova-plugin-file
 cordova plugin add https://github.com/apache/cordova-plugin-file-transfer#ec11d2fd0e87d7d9e0ba94b1989ffb71af1c3c96 --save
+# install the native logs plugin
+cordova plugin add https://github.com/CodeWithOz/cordova-plugin-native-logs#dda6116 --save
