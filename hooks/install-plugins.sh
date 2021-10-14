27f9795481ba61e9e34e155ce3215c0a871109c7
Uche Ozoemena <ucheozoemena@gmail.com>
2021-10-14 01:23:36 +0100

    feat(core): install a different version of the maps plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -4,4 +4,4 @@ cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 
 # install google maps plugin
-cordova plugin add cordova-plugin-googlemaps@2.7.1 --save
+cordova plugin add cordova-plugin-googlemaps --save
