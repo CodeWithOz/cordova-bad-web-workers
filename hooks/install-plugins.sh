aa8aaa30a2cab4fb1828d248b9d40c7f8f820a30
Uche Ozoemena <ucheozoemena@gmail.com>
2021-11-05 02:00:56 +0100

    feat(core): install the native storage plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -4,5 +4,5 @@ cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 cordova plugin add cordova-plugin-file
 cordova plugin add https://github.com/apache/cordova-plugin-file-transfer#ec11d2fd0e87d7d9e0ba94b1989ffb71af1c3c96 --save
-# install the native logs plugin
-cordova plugin add https://github.com/CodeWithOz/cordova-plugin-native-logs#dda6116 --save
+# install the native storage plugin
+cordova plugin add https://github.com/CodeWithOz/cordova-plugin-nativestorage#0ccdd0d6f4a39ed0d95832fade6fc6ed1b09587b --save
