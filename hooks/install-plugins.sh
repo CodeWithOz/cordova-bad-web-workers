24e24af29150f7ca5997e4090145920df9976221
Uche Ozoemena <ucheozoemena@gmail.com>
2021-11-13 18:08:33 +0100

    feat(core): install the hardware accel plugin from local

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -7,4 +7,4 @@ cordova plugin add cordova-sqlite-storage@6.0.0 --save
 # install the native storage plugin
 cordova plugin add https://github.com/CodeWithOz/cordova-plugin-nativestorage#0ccdd0d6f4a39ed0d95832fade6fc6ed1b09587b --save
 # install the hardware acceleration plugin
-cordova plugin add https://github.com/Textras-Inc/cordova-plugin-hardware-acceleration.git --save
+cordova plugin add ./custom-plugins/cordova-plugin-hardware-acceleration --save
