d619d851a3a29d78035c258c9945d2d1d747482c
Uche Ozoemena <ucheozoemena@gmail.com>
2021-12-02 13:18:11 +0100

    feat(core): add blurhash

--- www/js/modules/test.js
+++ www/js/modules/test.js
@@ -1,4 +1,6 @@
 import { printer } from './exporter.js';
+import blurhash from './blurhash';
 printer('this is the modular test');
 
 window.printerModule = printer;
+window.blurhashModule = blurhash;
