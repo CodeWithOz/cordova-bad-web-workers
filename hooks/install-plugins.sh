dbaf95d55133fb1bb324a643e96f3dc59dce35da
Uche Ozoemena <ucheozoemena@gmail.com>
2021-12-15 00:31:58 +0100

    feat(core): install the rabbitmq plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -8,3 +8,6 @@ cordova plugin add cordova-sqlite-storage@6.0.0 --save
 cordova plugin add https://github.com/CodeWithOz/cordova-plugin-nativestorage#0ccdd0d6f4a39ed0d95832fade6fc6ed1b09587b --save
 # install the hardware acceleration plugin
 cordova plugin add ./custom-plugins/cordova-plugin-hardware-acceleration --save
+# install the amqp plugin
+cordova plugin add https://github.com/xiaoji-duan/cordova-plugin-rabbitmq#ebba54a5ba44c770462c2d68f1442879bb830701 --save
+
