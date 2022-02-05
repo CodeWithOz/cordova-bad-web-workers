d85d029f4873d97dd0908955c11e43895b57da7e
Uche Ozoemena <ucheozoemena@gmail.com>
2022-02-06 00:41:48 +0100

    feat(core): update code to fix build failures

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cordova plugin add cordova-plugin-whitelist@1.3.4
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
