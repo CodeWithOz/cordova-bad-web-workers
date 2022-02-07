86d86658a1e5a3af4f7b371a45457a88861aaaac
Uche Ozoemena <ucheozoemena@gmail.com>
2022-02-07 08:55:06 +0100

    feat(core): add a log statement

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        console.log('[app] deviceready fired');
         let myPane;
         let topHeight = 750;
         let scrollTop;
