88297c11f56f340af9d2c5f7862d1017d75b23c7
Uche Ozoemena <ucheozoemena@gmail.com>
2022-02-16 12:03:01 +0100

    feat(core): install the sqlite plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -2,3 +2,5 @@
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
+# install sqlite plugin
+cordova plugin add cordova-sqlite-storage@6.0.0 --save
