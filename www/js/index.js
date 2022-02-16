12fe784fc7614d1b83d6607c2b74c8b57a87199c
Uche Ozoemena <ucheozoemena@gmail.com>
2022-02-16 13:12:31 +0100

    feat(core): make the button run the window function

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,9 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         console.log('[app] deviceready fired');
+        document.querySelector('.toggle').addEventListener('click', e => {
+            app.performWindowFunction();
+        });
         app.getDB('repro.db', db => {
             // operations are reproduced and working at https://dbfiddle.uk/?rdbms=sqlite_3.27&fiddle=da10568e4e8ad57699fa89617b2dcfee
             const operations = [
@@ -38,30 +41,57 @@ var app = {
                     ('alternateChat', '0001-03-01T01:01:11.001Z', '28c9bb49-09f4-4dc4-b0ba-a3c45c324348', 'third message', 'first_sender_id', 'groupedMsgs'),
                     ('alternateChat', '0001-04-01T01:01:11.001Z', '18c9bb49-09f5-4dc4-b0ba-a3c45c324348', 'fourth message', 'first_sender_id', 'txt'),
                     ('alternateChat', '0001-05-01T01:01:11.001Z', '18c9bb49-19f4-4dc4-b0ba-a3c45c324348', 'fifth message', 'first_sender_id', 'groupedMsgs');`,
-                `WITH cte AS (
-                    SELECT *,
-                        COUNT(*) OVER (PARTITION BY grp) count,
-                        ROW_NUMBER() OVER (PARTITION BY grp ORDER BY date) rn
-                    FROM (
-                        SELECT *, SUM(flag) OVER (ORDER BY date) grp
-                        FROM (
-                            SELECT *, (type <> LAG(type, 1, '') OVER (ORDER BY date)) flag
-                            FROM chatMessages WHERE chatID = 'alternateChat'
-                        )
-                    )
-                )
-                SELECT id, type, date, message,
-                    CASE WHEN type = 'groupedMsgs' THEN count END numConsecutiveItems
-                FROM cte
-                WHERE numConsecutiveItems IS NULL OR rn = 1;`,
             ];
-            db.sqlBatch(operations, res => {
-                console.log('operations outcome:', res);
-                alert(`operations outcome: ${res}`);
-            }, err => {
-                alert(`failed to complete operations, err: ${err.message}`);
-                console.error(err);
-            });
+            db.sqlBatch(
+                operations,
+                res => {
+                    console.log('operations outcome:', res);
+                    app.performWindowFunction();
+                },
+                err => {
+                    alert(`failed to complete operations, err: ${err.message}`);
+                    console.error(err);
+                }
+            );
+        }, err => {
+            alert(`failed to get database, err: ${err.message}`);
+            console.error(err);
+        });
+    },
+    performWindowFunction: () => {
+        app.getDB('repro.db', db => {
+            // operations are reproduced and working at https://dbfiddle.uk/?rdbms=sqlite_3.27&fiddle=da10568e4e8ad57699fa89617b2dcfee
+            db.executeSql(
+                `WITH cte AS (
+            SELECT *,
+                COUNT(*) OVER (PARTITION BY grp) count,
+                ROW_NUMBER() OVER (PARTITION BY grp ORDER BY date) rn
+            FROM (
+                SELECT *, SUM(flag) OVER (ORDER BY date) grp
+                FROM (
+                    SELECT *, (type <> LAG(type, 1, '') OVER (ORDER BY date)) flag
+                    FROM chatMessages WHERE chatID = 'alternateChat'
+                )
+            )
+        )
+        SELECT id, type, date, message,
+            CASE WHEN type = 'groupedMsgs' THEN count END numConsecutiveItems
+        FROM cte
+        WHERE numConsecutiveItems IS NULL OR rn = 1;`,
+                [],
+                res => {
+                    console.log('num results:', res.rows.length);
+                    alert(
+                        `operations outcome, num rows: ${res.rows.length}`
+                    );
+                },
+                err => {
+                    alert(
+                        `failed to perform window function, err: ${err.message}`
+                    );
+                    console.error(err);
+                }
+            );
         }, err => {
             alert(`failed to get database, err: ${err.message}`);
             console.error(err);
