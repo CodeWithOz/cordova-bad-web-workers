a2546bed8f011ba5adb6010b374c85a1cf3b8655
Uche Ozoemena <ucheozoemena@gmail.com>
2022-04-02 18:47:09 +0100

    feat(core): copy over the webworker deps properly

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -4,14 +4,12 @@
 npm install
 
 # bundle js
-mkdir www/js
 npm run build
 
 # copy over the src files that don't need processing
 cp -r src/index.html www/
 cp -r src/js/index.js www/js
-mkdir www/js/workers/deps
-cp -r src/js/workers/deps/*.js www/js/workers/deps
+cp -r src/js/workers/deps/ www/js/workers/
 
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
