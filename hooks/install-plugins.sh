3a32278f5c0dae6018a5b3ce335a0cfcb557ad44
Uche Ozoemena <ucheozoemena@gmail.com>
2022-04-02 16:09:34 +0100

    feat(core): only copy over the src files that don't need processing

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -4,10 +4,12 @@
 npm install
 
 # bundle js
+mkdir www/js
 npm run build
 
-# copy over the src files
-cp -r src/* www/
+# copy over the src files that don't need processing
+cp -r src/index.html www/
+cp -r src/js/index.js www/js
 
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
