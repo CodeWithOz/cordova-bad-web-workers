73b7871ad14dc4a91ff089215cabd0f1d69a86e1
Uche Ozoemena <ucheozoemena@gmail.com>
2022-04-02 10:58:58 +0100

    feat(core): copy over the src files into the www folder at build time

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# copy over the src files
+cp -r src/* www/
+
 # install androidx adapter plugins to workaround build failures
 cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
