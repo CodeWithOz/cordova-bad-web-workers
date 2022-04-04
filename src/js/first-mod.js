59cd024266cc8018be6ef32b5f8b64028ab8fcd9
Uche Ozoemena <ucheozoemena@gmail.com>
2022-04-04 11:29:32 +0100

    feat(core): add logic to get and show value of a variable

--- src/js/first-mod.js
+++ src/js/first-mod.js
@@ -1,3 +1,5 @@
 export function firstModFunc1() {
     console.log('running first func of first mod');
 }
+
+export var state = null;
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { secondModFunc1, secondModFunc2 } from './second-mod';
 import { thirdModFunc1, thirdModFunc2 } from './third-mod';
-import { getWorker, destroyWorker } from './workers/helpers';
+import { state } from './first-mod';
 
 export function initAppBeforeDeviceReady() {
     console.log('[CUSTOM LOG] app initialized before deviceready fires');
@@ -15,68 +15,18 @@ export function initAppAfterDeviceReady() {
     document
         .querySelector('.init-bottom-sheet')
         .addEventListener('click', e => {
-            const workerAndProxy = getWorker('first');
-            Promise.all([
-                new Promise(resolve => {
-                    workerAndProxy[0]
-                        .testWorker('first val')
-                        .then(val => {
-                            alert(`first normal test value: ${val}`);
-                            resolve();
-                        })
-                        .catch(err => {
-                            console.log(
-                                `error for first test:`,
-                                JSON.stringify(err)
-                            );
-                            alert(
-                                `error for first test: ${JSON.stringify(err)}`
-                            );
-                            resolve();
-                        });
-                }),
-                new Promise(resolve => {
-                    workerAndProxy[0]
-                        .testDayjs(Date.now() - Math.random() * 1000000000)
-                        .then(val => {
-                            alert(`dayjs compared values: ${val}`);
-                            resolve();
-                        })
-                        .catch(err => {
-                            console.log(
-                                `error for second test:`,
-                                JSON.stringify(err)
-                            );
-                            alert(
-                                `error for second test: ${JSON.stringify(err)}`
-                            );
-                            resolve();
-                        });
-                }),
-            ])
-                .then(() => {
-                    alert(`finished worker tests, destroying worker`);
-                    destroyWorker(workerAndProxy);
-                })
-                .catch(err => {
-                    alert(
-                        `error from Promise.all test: ${JSON.stringify(err)}`
-                    );
-                    destroyWorker(workerAndProxy);
-                });
+            if (state) {
+                alert(`state.fakeProp: ${state.fakeProp}`);
+            } else {
+                alert(`state: ${state}`);
+            }
         });
     document
         .querySelector('.destroy-bottom-sheet')
         .addEventListener('click', e => {
-            import('./pages/test/index.js')
-                .then(({ pageFunc }) => {
-                    pageFunc('testing page func');
-                })
-                .catch(err => {
-                    console.log(`error importing "test" page`, err);
-                    alert(
-                        `error importing "test" page: ${JSON.stringify(err)}`
-                    );
-                });
+            const newVal = Math.floor(Math.random() * 1000);
+            alert(`setting state.fakeProp to ${newVal}`);
+            state.fakeProp = newVal;
+            alert(`setting state.fakeProp: ${state.fakeProp}`);
         });
 }
