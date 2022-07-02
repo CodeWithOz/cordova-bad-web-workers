db7894cf938dd644376a9bed8909f3efe8d4df68
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-02 01:11:16 +0100

    feat(core): do not install the androidx plugin

--- hooks/install-plugins.sh
+++ hooks/install-plugins.sh
@@ -13,7 +13,7 @@ cp -r src/js/workers/deps/ www/js/workers/
 rsync -a --prune-empty-dirs --include '*/' --exclude '*.js' src/js/ www/js
 
 # install androidx adapter plugins to workaround build failures
-cordova plugin add cordova-plugin-androidx --save
+# cordova plugin add cordova-plugin-androidx --save
 cordova plugin add cordova-plugin-androidx-adapter --save
 # install splashscreen plugin
 cordova plugin add cordova-plugin-splashscreen@6.0.1 --save
